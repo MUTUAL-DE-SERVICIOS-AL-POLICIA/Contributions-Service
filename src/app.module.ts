f3e9d023ef33398393caf24fe1b13104675cfbdf
dilan <dilantorrezsalinas@gmail.com>
2025-12-31 12:21:51 -0400

    servicio contributions creado para obtener contribuciones de afiliados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
+import { ContributionsModule } from './contributions/contributions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),DatabaseModule , CommonModule],
+  imports: [ConfigModule.forRoot(),DatabaseModule , CommonModule, ContributionsModule],
   controllers: [],
   providers: [],
 })
